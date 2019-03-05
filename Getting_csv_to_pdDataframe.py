57b5e09ba92ab441076ddff49a51d6ee1ac709a8
Rahul Malhotra <r_malhot@encs.concordia.ca>
2019-03-05 14:57:59 -0500

    Initial Commit

---
+++ Getting_csv_to_pdDataframe.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import io
+import time
+import sys
+import requests
+from fake_useragent import UserAgent
+ua = UserAgent()
+hdr = {'User-Agent':str(ua.chrome)}
+
+with requests.Session() as s:
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+    page = s.get(url, headers=hdr)
+cont=page.content
+print(cont)
+print(type(cont))
+
+df = pd.read_csv(io.StringIO(cont.decode('utf-8')), sep=',',header=0, names=['sepal length(cm)','sepal width(cm)','petal length(cm)','petal width(cm)',"class",])
+print(df)
+writer = pd.ExcelWriter('IrisData.xlsx')
+df.to_excel(writer, sheet_name='Sheet')
+writer.save()
